31000e6879aee53c33450a697fe33a2f7ec6c4bd
rajatsangrame <33744237+rajatsangrame@users.noreply.github.com>
2019-07-30 14:14:04 +0530

    Update setup-nvidia-410.sh

--- setup-nvidia-410.sh
+++ setup-nvidia-410.sh
@@ -42,7 +42,7 @@ sudo apt install unzip -y
 
 sudo add-apt-repository ppa:graphics-drivers/ppa
 sudo apt update
-sudo apt install nvidia-driver-410
+sudo apt install nvidia-driver-410 libnvidia-gl-410 nvidia-utils-410 xserver-xorg-video-nvidia-410 libnvidia-cfg1-410 libnvidia-ifr1-410 libnvidia-decode-410 libnvidia-encode-410
 
 sudo nvidia-smi
 
